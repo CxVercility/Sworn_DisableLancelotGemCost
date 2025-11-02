7ec748ba2d6e852d73d5e665f10d9066bcc5f0ef
CxVercility <vercility@gmail.com>
2025-11-02 16:24:51 +0100

    Add project files.

---
+++ SwornMod2/Core.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+
+[assembly: MelonInfo(typeof(Sworn_GemNoGoldCost.Core), "Sworn_GemNoGoldCost", "1.0.0", "Vercility", null)]
+[assembly: MelonGame("Windwalk Games", "SWORN")]
+
+namespace Sworn_GemNoGoldCost
+
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("Initialized.");
+        }
+    }
+}
+[HarmonyPatch(typeof(CurrencyManager), "AddGold")]
+class Patch
+{
+    static Boolean hasRun = false;
+    static bool Prefix(int amount)
+    {
+        if (!hasRun && amount == -300)
+        {
+            MelonLogger.Msg("skipped");
+            hasRun = true;
+            return false;
+        }
+        return true;
+    }
+
+}
